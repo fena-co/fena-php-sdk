9fd755dbe4cab0e5d43a212ca556ea208ab8b0d9
Alexandr Tsepushelov <alex.tsepush@gmail.com>
2022-08-30 18:46:05 +0400

    SDK v0.1.3

--- src/Payment.php
+++ src/Payment.php
@@ -9,17 +9,20 @@ class Payment
     private $endpoint = 'https://business.api.fena.co/public/payments/create-and-process';
 
     protected $refNumber;
+    protected $orderId;
     protected $amount;
     protected $user;
     protected $provider;
     protected $items = [];
     protected $deliveryAddress;
+    protected $reference = null;
 
 
     /**
      * @param Connection $connection
+     * @param string $orderId client order id
      * @param string $amount amount in 2 decimal places
-     * @param string $reference reference number for payment
+     * @param string|null $reference reference number for payment
      * @return Error|Payment
      */
     public static function createPayment(
@@ -38,35 +41,31 @@ class Payment
             return new Error(Errors::CODE_5);
         }
 
-        // validate order is greater than 255
-
+        // validate order id is greater than 18
         if (strlen($reference) > 18) {
             return new Error(Errors::CODE_6);
         }
 
-        if (!self::validateReference($reference)) {
-            return new Error(Errors::CODE_28);
-        }
-
-        return new Payment($connection, $amount, $reference);
+        return new Payment($connection, $reference, $amount);
     }
 
     /**
      * Payment constructor.
      * @param Connection $connection
+     * @param $orderId string unique order id
      * @param $amount  string amount requested
-     * @param $orderId string reference number for payment
+     * @param string|null $reference reference number for payment
      */
     private function __construct(
-        Connection $connection,
-        string     $orderId,
-        string     $amount
-    )
-    {
-        $this->connection = $connection;
-        $this->refNumber = $orderId;
-        $this->amount = $amount;
-    }
+            Connection $connection,
+            string     $orderId,
+            string     $amount
+        )
+        {
+            $this->connection = $connection;
+            $this->refNumber = $orderId;
+            $this->amount = $amount;
+        }
 
 
     /**
@@ -114,18 +113,6 @@ class Payment
     }
 
     /**
-     * @param $reference
-     * @return bool
-     */
-    private static function validateReference($reference): bool
-    {
-        if (!is_string($reference) || (preg_match('/^[\w-]+$/', $reference) !== 1)) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
      * process the payment
      * @return Error|string
      */
@@ -148,21 +135,21 @@ class Payment
         if ($this->deliveryAddress instanceof DeliveryAddress) {
             $payload['deliveryAddress'] =
                 [
-                    'addressLine1' => $this->deliveryAddress->getAddressLine1(),
-                    'addressLine2' => $this->deliveryAddress->getAddressLine2(),
-                    'zipCode' => $this->deliveryAddress->getPostCode(),
-                    'city' => $this->deliveryAddress->getCity(),
-                    'country' => $this->deliveryAddress->getCountry()
+                    'addressLine1' => (string)$this->deliveryAddress->getAddressLine1(),
+                    'addressLine2' => (string)$this->deliveryAddress->getAddressLine2(),
+                    'zipCode' => (string)$this->deliveryAddress->getPostCode(),
+                    'city' => (string)$this->deliveryAddress->getCity(),
+                    'country' => (string)$this->deliveryAddress->getCountry()
                 ];
         }
 
         $integrationId = $this->connection->getIntegrationId();
         $integrationSecret = $this->connection->getIntegrationSecret();
-        $headers = ['Content-Type: application/json', 'secret_key: ' . $integrationSecret, 'integration_id: ' . $integrationId];
+        $headers = array('Content-Type: application/json', "secret-key: {$integrationSecret}", "integration-id: {$integrationId}");
 
         curl_setopt($curl, CURLOPT_URL, $this->endpoint);
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_HEADER, $headers);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
 
@@ -175,6 +162,7 @@ class Payment
             // Decoding JSON data
             $decodedData =
                 json_decode($response, true);
+
 
             if ($decodedData['created'] === true) {
                 return $decodedData['result']['link'];
